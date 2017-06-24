8aa7c5659a3763820017609200060a3562a4bbbd
Benjamin Armintor <armintor@gmail.com>
2017-06-24 14:22:41 -0400

    capture past plural (were soldiers)

--- bin/egrep-soldier-assert-past.sh
+++ bin/egrep-soldier-assert-past.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pcregrep -i -M "\b(was|were)([\s,()]+not?)?([\s,()]+(a|thier|my|y?our))?[\s,()]+([a-z]+-)?soldier\b" $1/*
-pcregrep -i -M "\b(ha(ve|s)\s(not\s)?b(ee|i)n)[\s,()]([\s,()]+not?)?([\s,()]+(a|thier|my|y?our))?[\s,()]+([a-z]+-)?soldier\b" $1/*
+pcregrep -i -M "\b(was|were)([\s,()]+not?)?([\s,()]+(a|thier|my|y?our))?[\s,()]+([a-z]+-)?soldiers?\b" $1/*
+pcregrep -i -M "\b(ha(ve|s)\s(not\s)?b(ee|i)n)[\s,()]([\s,()]+not?)?([\s,()]+(a|thier|my|y?our))?[\s,()]+([a-z]+-)?soldiers?\b" $1/*
