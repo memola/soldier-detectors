f7d2db19901a3b0d3dbe7b089671d21855bf57ee
Benjamin Armintor <armintor@gmail.com>
2017-06-28 23:10:57 -0400

    refactor around shared patterns and stricter separation of plurals

--- bin/egrep-gentleman-assert.sh
+++ bin/egrep-gentleman-assert.sh
@@ -1,42 +1,6 @@
 #!/bin/bash
-# egrep -e "(is|are|am)\s[Aa]\s([A-Za-z]+-)?(bachelor|gentleman)" ReprocessedSimpleText/*
-# egrep -e "\b(is|are|am)\s([A-Za-z]+-)?(bachelor|gentleman)" ReprocessedSimpleText/*
-titles="bachelor|gentlem[ae]n|scholar|courtier"
-# singular assertions
-# not "it", "is" or "am", optional "a", optionally prefixed "(bachelor|gentleman)""(bachelor|gentleman)"
-pcregrep -i -M "(?<!it)[\W]+(is|am)([\s,()]+not?)?([\s,()]+a)?[\s,()]+([a-z]+-)?(${titles})[s-]?\b" $1/*
-
-# "are", optional "no" or "not", optional "a", optionally prefixed "(bachelor|gentleman)"
-pcregrep -i -M "\b(ar[et])([\s,()]+not?)?([\s,()]+a)?[\s,()]+([a-z]+-)?(${titles})[s-]?\b" $1/*
-
-# "be", optional "no" or "not", optional "a", "(bachelor|gentleman)" 
-pcregrep -i -M "\b(be)([\s,()]+not?)?([\s,()]+a)?[\s,()]+([a-z]+-)?(${titles})[s-]?\b" $1/*
-
-# implied verb, optional "no" or "not", some word,  optional "a", optionally prefixed "(bachelor|gentleman)" 
-#pcregrep -i -M "\b(I|you|s?he|thou|[w|y]e|they)([\s,()]+[a-zA-Z\-]+)?([\s,()]+not?)?([\s,()]+a?)?[\s,()]+(bachelor|gentleman)[s-]?\b" $1/*
-
-# implied verb, optional "no" or "not",  optional "a", optionally prefixed "(bachelor|gentleman)" 
-pcregrep -i -M "\b(I|you|s?he|thou|[w|y]e|they)([\s,()]+not?)?([\s,()]+a)?[\s,()]+([a-z]+-)?(${titles})[s-]?\b" $1/*
-
-# seeming
-pcregrep -i -M "\bseems?([\s,()]+not?)?([\s,()]+a)?[\s,()]+(${titles})s?\b" $1/*
-
-# plural
-# "are", optional "no" or "not", (0-1 words), "(bachelor|gentleman)s"
-pcregrep -i -M "\b(are)([\s,()]+not?)?[\s,()]+([a-z]+-)?(${titles})[s-]?\b" $1/*
-
-# two states
-pcregrep -i -M "I[\s,()]+(am)([\s,()]+an?)?[\s,()]+\w+([A-z]+[\s,()]+){0,5}a\s(${titles})[s-]?\b" $1/*
-
-# contractions
-pcregrep -i -M "I[\s]*'?me?([\s,()]+a?)?[\s,()]+(${titles})s?\b" $1/*
-pcregrep -i -M "s?he[\s]*'?s([\s,()]+a?)?[\s,()]+(${titles})s?\b" $1/*
-pcregrep -i -M "thou[\s]*'?rt([\s,()]+a?)?[\s,()]+(${titles})s?\b" $1/*
-pcregrep -i -M "th[\s]*'?art([\s,()]+a?)?[\s,()]+(${titles})s?\b" $1/*
-pcregrep -i -M "they[\s]*'?re?([\s,()]+a?)?[\s,()]+(${titles})s?\b" $1/*
-pcregrep -i -M "you[\s]*'re?([\s,()]+a?)?[\s,()]+(${titles})s?\b" $1/*
-pcregrep -i -M "youre?([\s,()]+a)[\s,()]+(${titles})s?\b" $1/*
-
-# past states
-pcregrep -i -M "\b(was|were)([\s,()]+not?)?([\s,()]+(a|thier|my|y?our))?[\s,()]+([a-z]+-)?(${titles})s?\b" $1/*
-pcregrep -i -M "\b(ha(ve|s)\s(not\s)?b(ee|i)n)[\s,()]([\s,()]+not?)?([\s,()]+(a|thier|my|y?our))?[\s,()]+([a-z]+-)?(${titles})s?\b" $1/*
+thisdir=`dirname $0`
+SINGULAR="bachelor|gentleman|scholar|[A-Za-z]+er(?<!soldier)"
+PLURAL="bachelors|gentlemen|scholars|[A-Za-z]+ers(?<!soldiers)"
+. $thisdir/egrep-assert-present.cmds
+. $thisdir/egrep-assert-past.cmds
--- bin/egrep-soldier-assert-past.sh
+++ bin/egrep-soldier-assert-past.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-pcregrep -i -M "\b(was|were)([\s,()]+not?)?([\s,()]+a)?[\s,()]+([a-z]+-)?soldiers?\b" $1/*
-# have [not] I been [not]
-pcregrep -i -M "\b(ha(ve|d|s|th))([\s,()]+not?)?([\s,()]+(I|(th|y)ou|we|s?he|they))?[\s,()]+b(ee?|i)n([\s,()]+not?)?([\s,()]+a)?[\s,()]+([a-z]+-)?soldiers?\b" $1/*
-# have I [not] been [not]
-pcregrep -i -M "\b(ha(ve|d|s|th))([\s,()]+(I|(th|y)ou|we|s?he|they))?([\s,()]+not?)?[\s,()]+b(ee?|i)n([\s,()]+not?)?([\s,()]+a)?[\s,()]+([a-z]+-)?soldiers?\b" $1/*
-# have [not] been [not]
-pcregrep -i -M "\b(ha(ve|d|s|th))([\s,()]+not?)?[\s,()]+b(ee?|i)n([\s,()]+not?)?([\s,()]+a)?[\s,()]+([a-z]+-)?soldiers?\b" $1/*
+thisdir=`dirname $0`
+SINGULAR=soldier
+PLURAL=soldiers
+. $thisdir/egrep-assert-past.cmds
