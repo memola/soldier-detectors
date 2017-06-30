0ebc5720361677041c445cbfe76afcd96392ddd0
Benjamin Armintor <armintor@gmail.com>
2017-06-30 00:02:36 -0400

    more stringent contraction match for you are

--- bin/egrep-gentleman-assert.sh
+++ bin/egrep-gentleman-assert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 thisdir=`dirname $0`
-SINGULAR='bachelor|gentleman|scholar|([A-Za-z]+er(?<!soldier))'
+SINGULAR='bachelor|gentleman|scholar|([A-Za-z]+er(?<!soldier|never|ever))'
 PLURAL='bachelors|gentlemen|scholars|([A-Za-z]+ers(?<!soldiers))'
 . $thisdir/egrep-assert-present.cmds
 . $thisdir/egrep-assert-past.cmds
