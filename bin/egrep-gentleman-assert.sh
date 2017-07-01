1d72cd5684733679f2c9816c4e20fc98ebf29cae
Benjamin Armintor <armintor@gmail.com>
2017-07-01 19:06:45 -0400

    exclude matter and better from occupation position

--- bin/egrep-gentleman-assert.sh
+++ bin/egrep-gentleman-assert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 thisdir=`dirname $0`
-SINGULAR='bachelor|gentleman|scholar|([A-Za-z]+er(?<!soldier|never|ever))'
-PLURAL='bachelors|gentlemen|scholars|([A-Za-z]+ers(?<!soldiers))'
+SINGULAR='bachelor|gentleman|scholar|([A-Za-z]+er(?<!soldier|never|ever|(ma|be)tter))'
+PLURAL='bachelors|gentlemen|scholars|([A-Za-z]+ers(?<!soldiers|(ma|be)tters))'
 . $thisdir/egrep-assert-present.cmds
 . $thisdir/egrep-assert-past.cmds
